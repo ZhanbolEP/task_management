faada57b63b0ce7a731a62b5e73ad881975e9b71
Zhanbol <bollzhan4@gmail.com>
2024-10-17 13:32:05 +0200

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://bollzhan4:Z1h2a3n4b5o6l7n8@untitled.3hbsy.mongodb.net/?retryWrites=true&w=majority&appName=untitled',
+    jwt: 'dev-jwt'
+}
---
+++ controllers/order.js
@@ -0,0 +1,19 @@
+const order = require('../models/Order')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getAll = function(req, res) {
+
+    
+}
+module.exports.create = async function(req, res) {
+    try {
+        const order = await new Order({
+            list: req.body.list,
+            user: req.user.id,
+            order: 
+        }).save()
+        res.status(201).json(order)
+    } catch(e) {
+        errorHandler(res, e)
+    }
+}
